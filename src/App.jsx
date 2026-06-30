ad505dbc453cfade441e2505c6d0dc59982b8838
Matheus Biesdorf <122486659+matheusbiesdorf@users.noreply.github.com>
2026-06-30 00:00:28 -0400

    Limpando o projeto e primeira seção

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <section className="hero-section">
+        <div className="hero-overlay"></div>
+
+        <div className="hero-content">
+          <div className="hero-text">
+            <p className="hero-label">Portfólio</p>
+
+            <h1>
+              Olá, eu sou <span>Matheus</span>
+            </h1>
+
+            <h2>Estudante de Ciência da Computação</h2>
+
+            <p className="hero-description">
+              Sou desenvolvedor em formação, com interesse em desenvolvimento web, mobile e UX/UI.
+              Tenho experiência com tecnologias como React, TypeScript, JavaScript, HTML, CSS e
+              Figma, criando interfaces funcionais, responsivas e pensadas na experiência do
+              usuário.
+            </p>
+          </div>
+
+          <div className="hero-image-container">
+            <img src="/minha-foto.jpg" alt="Foto de Matheus" className="hero-image" />
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
